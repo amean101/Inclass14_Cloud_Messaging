db220814ab97389681e28b63e48cf5ac456f2af0
Andy Mean <andymean8@gmail.com>
2025-04-09 22:46:26 -0400

    added message importance checker and displaying messages from firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "com.example.act14"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk 19
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -45,3 +45,6 @@ android {
 flutter {
     source = "../.."
 }
+
+apply(plugin = "com.google.gms.google-services") // ✅ Required for Firebase
+
